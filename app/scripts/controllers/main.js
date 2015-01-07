25cb825cc06620fca6f082f6f8a865539bce8124
ywarezk <ywarezk@gmail.com>
2015-01-07 13:47:48 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularLeafletApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
